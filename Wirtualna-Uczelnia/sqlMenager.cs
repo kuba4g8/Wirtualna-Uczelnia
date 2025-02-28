b8f74d72e576b18e0d960338c62834c32fa662de
kuba4g8 <kuba.holdys@gmail.com>
2025-02-28 11:21:34 +0100

    Rozwiazanie

---
+++ Wirtualna-Uczelnia/sqlMenager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Wirtualna_Uczelnia
+{
+    public class sqlMenager
+    {
+        MySqlConnection _conn; // obiekt klasy umozliwiajacy laczenie sie z baza danych.
+
+        //
+
+    }
+}
