da45878d9d6e96ada481ec6403f7e6e5c456bfa8
kuba4g8 <kuba.holdys@gmail.com>
2025-02-28 11:31:58 +0100

    Pliki TEMP

---
+++ Wirtualna-Uczelnia-Rozwiazanie/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Wirtualna_Uczelnia
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnLogin = new Button();
+            txtLogin = new TextBox();
+            txtPassword = new TextBox();
+            lblLogin = new Label();
+            lblPass = new Label();
+            SuspendLayout();
+            // 
+            // btnLogin
+            // 
+            btnLogin.Location = new Point(12, 116);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(222, 29);
+            btnLogin.TabIndex = 0;
+            btnLogin.Text = "zzaloguj";
+            btnLogin.UseVisualStyleBackColor = true;
+            btnLogin.Click += btnLogin_Click;
+            // 
+            // txtLogin
+            // 
+            txtLogin.Location = new Point(12, 12);
+            txtLogin.Name = "txtLogin";
+            txtLogin.Size = new Size(222, 27);
+            txtLogin.TabIndex = 1;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(12, 67);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(222, 27);
+            txtPassword.TabIndex = 2;
+            // 
+            // lblLogin
+            // 
+            lblLogin.AutoSize = true;
+            lblLogin.Location = new Point(240, 15);
+            lblLogin.Name = "lblLogin";
+            lblLogin.Size = new Size(56, 20);
+            lblLogin.TabIndex = 3;
+            lblLogin.Text = "- Login";
+            // 
+            // lblPass
+            // 
+            lblPass.AutoSize = true;
+            lblPass.Location = new Point(240, 70);
+            lblPass.Name = "lblPass";
+            lblPass.Size = new Size(80, 20);
+            lblPass.TabIndex = 4;
+            lblPass.Text = "- Password";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(359, 170);
+            Controls.Add(lblPass);
+            Controls.Add(lblLogin);
+            Controls.Add(txtPassword);
+            Controls.Add(txtLogin);
+            Controls.Add(btnLogin);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnLogin;
+        private TextBox txtLogin;
+        private TextBox txtPassword;
+        private Label lblLogin;
+        private Label lblPass;
+    }
+}
---
+++ Wirtualna-Uczelnia-Rozwiazanie/Form1.cs
@@ -0,0 +1,15 @@
+namespace Wirtualna_Uczelnia
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
