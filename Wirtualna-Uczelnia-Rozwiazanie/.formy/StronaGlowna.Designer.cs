0073ff2ab34889304ee19691a166442c61c2b6f3
kuba4g8 <kuba.holdys@gmail.com>
2025-04-01 07:51:35 +0200

    male poprawki i usuniecie folderow

--- Wirtualna-Uczelnia-Rozwiazanie/.formy/StronaGlowna.Designer.cs
+++ Wirtualna-Uczelnia-Rozwiazanie/.formy/StronaGlowna.Designer.cs
@@ -51,8 +51,9 @@
             // 
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
             pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Margin = new Padding(2, 2, 2, 2);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(1903, 1080);
+            pictureBox1.Size = new Size(1522, 864);
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
             pictureBox1.Click += pictureBox1_Click;
@@ -62,10 +63,10 @@
             oceny.BackColor = Color.Transparent;
             oceny.BackgroundImageLayout = ImageLayout.None;
             oceny.Image = (Image)resources.GetObject("oceny.Image");
-            oceny.Location = new Point(612, 294);
+            oceny.Location = new Point(490, 235);
             oceny.Margin = new Padding(0);
             oceny.Name = "oceny";
-            oceny.Size = new Size(337, 176);
+            oceny.Size = new Size(270, 141);
             oceny.TabIndex = 1;
             oceny.UseVisualStyleBackColor = false;
             oceny.Click += oceny_Click;
@@ -73,9 +74,10 @@
             // sprawdziany
             // 
             sprawdziany.Image = (Image)resources.GetObject("sprawdziany.Image");
-            sprawdziany.Location = new Point(1010, 294);
+            sprawdziany.Location = new Point(808, 235);
+            sprawdziany.Margin = new Padding(2, 2, 2, 2);
             sprawdziany.Name = "sprawdziany";
-            sprawdziany.Size = new Size(337, 176);
+            sprawdziany.Size = new Size(270, 141);
             sprawdziany.TabIndex = 2;
             sprawdziany.UseVisualStyleBackColor = true;
             sprawdziany.Click += sprawdziany_Click;
@@ -83,9 +85,10 @@
             // dokumenty
             // 
             dokumenty.Image = (Image)resources.GetObject("dokumenty.Image");
-            dokumenty.Location = new Point(612, 522);
+            dokumenty.Location = new Point(490, 418);
+            dokumenty.Margin = new Padding(2, 2, 2, 2);
             dokumenty.Name = "dokumenty";
-            dokumenty.Size = new Size(337, 170);
+            dokumenty.Size = new Size(270, 136);
             dokumenty.TabIndex = 3;
             dokumenty.UseVisualStyleBackColor = true;
             dokumenty.Click += dokumenty_Click;
@@ -93,9 +96,10 @@
             // rejestracja
             // 
             rejestracja.Image = (Image)resources.GetObject("rejestracja.Image");
-            rejestracja.Location = new Point(1010, 522);
+            rejestracja.Location = new Point(808, 418);
+            rejestracja.Margin = new Padding(2, 2, 2, 2);
             rejestracja.Name = "rejestracja";
-            rejestracja.Size = new Size(337, 170);
+            rejestracja.Size = new Size(270, 136);
             rejestracja.TabIndex = 4;
             rejestracja.UseVisualStyleBackColor = true;
             rejestracja.Click += rejestracja_Click;
@@ -103,9 +107,10 @@
             // kalendarz
             // 
             kalendarz.Image = (Image)resources.GetObject("kalendarz.Image");
-            kalendarz.Location = new Point(612, 746);
+            kalendarz.Location = new Point(490, 597);
+            kalendarz.Margin = new Padding(2, 2, 2, 2);
             kalendarz.Name = "kalendarz";
-            kalendarz.Size = new Size(337, 170);
+            kalendarz.Size = new Size(270, 136);
             kalendarz.TabIndex = 5;
             kalendarz.UseVisualStyleBackColor = true;
             kalendarz.Click += kalendarz_Click;
@@ -113,9 +118,10 @@
             // imie_nazwisko
             // 
             imie_nazwisko.AutoSize = true;
-            imie_nazwisko.Location = new Point(270, 83);
+            imie_nazwisko.Location = new Point(216, 66);
+            imie_nazwisko.Margin = new Padding(2, 0, 2, 0);
             imie_nazwisko.Name = "imie_nazwisko";
-            imie_nazwisko.Size = new Size(135, 25);
+            imie_nazwisko.Size = new Size(113, 20);
             imie_nazwisko.TabIndex = 7;
             imie_nazwisko.Text = "imię i nazwisko:";
             imie_nazwisko.Click += imie_nazwisko_Click;
@@ -123,9 +129,10 @@
             // wydzial_kierunek
             // 
             wydzial_kierunek.AutoSize = true;
-            wydzial_kierunek.Location = new Point(270, 127);
+            wydzial_kierunek.Location = new Point(216, 102);
+            wydzial_kierunek.Margin = new Padding(2, 0, 2, 0);
             wydzial_kierunek.Name = "wydzial_kierunek";
-            wydzial_kierunek.Size = new Size(155, 25);
+            wydzial_kierunek.Size = new Size(129, 20);
             wydzial_kierunek.TabIndex = 8;
             wydzial_kierunek.Text = "wydział i kierunek:";
             wydzial_kierunek.Click += label2_Click;
@@ -133,9 +140,10 @@
             // semestr
             // 
             semestr.AutoSize = true;
-            semestr.Location = new Point(270, 174);
+            semestr.Location = new Point(216, 139);
+            semestr.Margin = new Padding(2, 0, 2, 0);
             semestr.Name = "semestr";
-            semestr.Size = new Size(192, 25);
+            semestr.Size = new Size(157, 20);
             semestr.TabIndex = 9;
             semestr.Text = "tryb studiów i semestr:";
             semestr.Click += semestr_Click;
@@ -143,9 +151,10 @@
             // pictureBox2
             // 
             pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
-            pictureBox2.Location = new Point(0, 246);
+            pictureBox2.Location = new Point(0, 197);
+            pictureBox2.Margin = new Padding(2, 2, 2, 2);
             pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(265, 780);
+            pictureBox2.Size = new Size(212, 624);
             pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
             pictureBox2.TabIndex = 10;
             pictureBox2.TabStop = false;
@@ -155,9 +164,10 @@
             // 
             pracownicy.AutoSize = true;
             pracownicy.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 238);
-            pracownicy.Location = new Point(24, 277);
+            pracownicy.Location = new Point(19, 222);
+            pracownicy.Margin = new Padding(2, 0, 2, 0);
             pracownicy.Name = "pracownicy";
-            pracownicy.Size = new Size(149, 32);
+            pracownicy.Size = new Size(123, 28);
             pracownicy.TabIndex = 11;
             pracownicy.Text = "- Pracownicy";
             // 
@@ -165,9 +175,10 @@
             // 
             stypendia.AutoSize = true;
             stypendia.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 238);
-            stypendia.Location = new Point(24, 376);
+            stypendia.Location = new Point(19, 301);
+            stypendia.Margin = new Padding(2, 0, 2, 0);
             stypendia.Name = "stypendia";
-            stypendia.Size = new Size(136, 32);
+            stypendia.Size = new Size(112, 28);
             stypendia.TabIndex = 12;
             stypendia.Text = "- Stypendia";
             // 
@@ -175,9 +186,10 @@
             // 
             plan.AutoSize = true;
             plan.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 238);
-            plan.Location = new Point(24, 327);
+            plan.Location = new Point(19, 262);
+            plan.Margin = new Padding(2, 0, 2, 0);
             plan.Name = "plan";
-            plan.Size = new Size(136, 32);
+            plan.Size = new Size(110, 28);
             plan.TabIndex = 13;
             plan.Text = "- Plan zajęć";
             // 
@@ -185,17 +197,18 @@
             // 
             grupy_zajeciowe.AutoSize = true;
             grupy_zajeciowe.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 238);
-            grupy_zajeciowe.Location = new Point(24, 427);
+            grupy_zajeciowe.Location = new Point(19, 342);
+            grupy_zajeciowe.Margin = new Padding(2, 0, 2, 0);
             grupy_zajeciowe.Name = "grupy_zajeciowe";
-            grupy_zajeciowe.Size = new Size(205, 32);
+            grupy_zajeciowe.Size = new Size(168, 28);
             grupy_zajeciowe.TabIndex = 14;
             grupy_zajeciowe.Text = "- Grupy zajęciowe";
             // 
             // StronaGlowna
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1898, 1024);
+            ClientSize = new Size(1518, 819);
             Controls.Add(grupy_zajeciowe);
             Controls.Add(plan);
             Controls.Add(stypendia);
@@ -210,6 +223,7 @@
             Controls.Add(sprawdziany);
             Controls.Add(oceny);
             Controls.Add(pictureBox1);
+            Margin = new Padding(2, 2, 2, 2);
             Name = "StronaGlowna";
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
