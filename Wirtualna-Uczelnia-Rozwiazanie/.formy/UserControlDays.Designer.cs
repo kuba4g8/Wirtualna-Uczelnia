2abbcd457d74a46ad70318d6e0e700dfdce3cadf
kuba4g8 <kuba.holdys@gmail.com>
2025-04-06 10:37:24 +0200

    Kalendarz wersja beta

---
+++ Wirtualna-Uczelnia-Rozwiazanie/.formy/UserControlDays.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace Kalendarz
+{
+    partial class UserControlDays
+    {
+        /// <summary> 
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod wygenerowany przez Projektanta składników
+
+        /// <summary> 
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować 
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lbdays = new Label();
+            SuspendLayout();
+            // 
+            // lbdays
+            // 
+            lbdays.AutoSize = true;
+            lbdays.Location = new Point(15, 12);
+            lbdays.Name = "lbdays";
+            lbdays.Size = new Size(32, 25);
+            lbdays.TabIndex = 0;
+            lbdays.Text = "00";
+            // 
+            // UserControlDays
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            Controls.Add(lbdays);
+            Name = "UserControlDays";
+            Size = new Size(149, 127);
+            Load += UserControlDays_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lbdays;
+    }
+}
